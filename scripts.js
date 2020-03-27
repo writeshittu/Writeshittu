cf3ca58b903ed95115698c1276e1a6efba1f7517
shit2boy <50172344+shit2boy@users.noreply.github.com>
2020-03-28 00:06:26 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+var name = "Akeem Shittu";
+var Courses = ["HTML", "CSS", "JAVASCRIPT"]
+console.log(name, Courses);
+var odd = []
+
+
+
+    var oddNumbers = function(nums) {
+        var nums= 1
+        for (var i = 0; i <= nums.length; i++) {
+                nums =+ 1;
+            if ((nums[i] % 2) != 0) {
+                odd.push(nums[i]);
+                    console.log(odd);
+            }
+           
+        }
+    
+    };
+
+console.log(oddNumbers);
