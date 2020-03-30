d308c48f5efcb13acadf60bc9a1f5e6a466cefe5
shit2boy <50172344+shit2boy@users.noreply.github.com>
2020-03-30 22:43:40 +0100

    Add files via upload

---
+++ nodeTask.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  if (req.url == '/message') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.message.path;
+      var newpath = 'message.txt' + files.message.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="message" method="post" >');
+    res.write('<input type="file" name="message"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080);
