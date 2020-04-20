a21c516611b468f9c93004fb377fd57dc021ba25
shit2boy <50172344+shit2boy@users.noreply.github.com>
2020-04-20 11:05:23 +0100

    Update jsTask3.js

--- jsTask3.js
+++ jsTask3.js
@@ -1,21 +1,21 @@
-function divisor (arr) {
-    let element;
-    for (const number in arr) {
-        if (number% 2 ===0 ) {
-            element = arr[number];
-            element = element.push("yu") 
-            
-        } 
-        if (number % 3 === 0) {
-            element = arr[number];
-            element = element.push("gi")
-
-        }
-        if (number % 5 === 0 ) {
-            element = arr[number];
-            element = element.push("oh")
-            
-        }
+function divisor(arr) {
+  for (let number in arr) {
+    if (arr[number] % 2 === 0) {
+      arr[number] = "yu";
     }
-    
+    if (arr[number] % 3 === 0) {
+      arr[number] = "gi";
+    }
+    if (arr[number] % 5 === 0) {
+      arr[number] = "oh";
+    }
+    if (arr[number] % 2 === 0 && arr[number] % 3 === 0) {
+      arr[number] = "yu-gi";
+    }
+    if (arr[number] % 3 === 0 && arr[number] % 5 === 0) {
+      arr[number] = "yu-gi-oh";
+    }
+  }
+  console.log(arr);
 }
+divisor([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15]);
